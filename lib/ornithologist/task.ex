a518d146df5e760450caa90a30510fd5df07cdd9
Nikita Naryshev <n.naryshev@mobidev.biz>
2019-10-30 21:00:48 +0200

    new and create task

--- lib/ornithologist/task.ex
+++ lib/ornithologist/task.ex
@@ -1,8 +1,19 @@
 defmodule Ornithologist.Task do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "tasks" do
+    field :name, :string
     field :description, :string
     field :priority, :integer
+  
+    timestamps()
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :description, :priority])
+    |> validate_required([:name, :description])
+    |> unique_constraint(:name)
   end
 end
--- priv/repo/migrations/20191029181427_create_tasks.exs
+++ priv/repo/migrations/20191029181427_create_tasks.exs
@@ -3,8 +3,11 @@ defmodule Ornithologist.Repo.Migrations.CreateTasks do
 
   def change do
     create table(:tasks) do
+      add :name, :string
       add :description, :string
       add :priority, :integer, default: 1
+
+      timestamps()
     end
   end
 end
